cf4043d4f8cac32842ec8e3c87f1172a46c5ab3d
YaroslavAnt <antonchyk.yar@gmail.com>
2020-01-12 15:39:44 +0200

    remove useless

--- pages/home/sections/popular.vue
+++ pages/home/sections/popular.vue
@@ -8,12 +8,6 @@
  
 <script>
 import sectionVue from "~/components/layout/section.vue";
-import but_3 from "~/assets/img/fence/but/but_3.jpg";
-import but_loza_3 from "~/assets/img/fence/but_loza/but_loza_3.jpg";
-import but_rovnyi_1 from "~/assets/img/fence/but_rovnyi/but_rovnyi_1.jpg";
-import fagot_2 from "~/assets/img/fence/fagot/fagot_2.jpg";
-import krym_1 from "~/assets/img/fence/krym/krym_1.jpg";
-import ImageBaseVue from "~/components/common/ImageBase.vue";
 import ProductCardVue from "~/components/common/ProductCard.vue";
 import { fence_set } from "~/static/fence_data/index";
 
@@ -21,7 +15,6 @@ export default {
   name: "popular.vue",
   components: {
     "app-section": sectionVue,
-    "app-image": ImageBaseVue,
     "product-card": ProductCardVue
   },
   data() {
