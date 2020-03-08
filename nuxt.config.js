c1d2fab54a446ea1bc386d9ea188a72b0f5f01b5
root <root@vps-33269.vps-default-host.net>
2020-03-08 21:55:20 +0200

    first pull

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,10 @@ export default {
   /*
    ** Headers of the page
    */
+    //server: {
+    //port: 3000, // default: 3000
+    //host: '0.0.0.0' // default: localhost
+  //},
   head: {
     htmlAttrs: {
       lang: "ru"
